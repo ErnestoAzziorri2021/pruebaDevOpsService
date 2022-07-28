ed702afd3c37f6b1b0fc2a1eb0b23e3b56a6a15e
ErnestoAzziorri2021 <ernestoazziorri@gmail.com>
2022-07-28 20:04:23 -0300

    otro test

--- src/main/java/uy/edu/ort/devops/productsserviceexample/ProductsServiceExampleApplication.java
+++ src/main/java/uy/edu/ort/devops/productsserviceexample/ProductsServiceExampleApplication.java
@@ -9,6 +9,7 @@ public class ProductsServiceExampleApplication {
 	public static void main(String[] args) {
 		System.out.println("Products Service Starting on Port 8080");
 		System.out.println("Test defensa");
+		System.out.println("Test defensa");
 		SpringApplication.run(ProductsServiceExampleApplication.class, args);
 	}
 
